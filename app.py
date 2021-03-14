f807f3fe5a457b0a77c1893034070f17ee8f6415
dominiccroce <dominiccroce@gmail.com>
2021-03-14 16:37:23 -0500

    implement pgConnection.py for TestUser

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import mongoconnection
 app = Flask(__name__)
 
 sample_mongodb_doc = mongoconnection.return_one()
+sample_pg = pgconnection.return_pg()
 
 @app.route('/')
 def hello():
-    return str(sample_mongodb_doc)
+    return str(sample_mongodb_doc) + "\n" + str(sample_pg)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- pgconnection.py
+++ pgconnection.py
@@ -1,3 +1,21 @@
 import psycopg2
 
-postgres_connection = psycopg2.connect(dbname="recipe_dev", user="postgres", password="postgres")
+def return_pg():
+    stringToReturn = None
+    try:
+        pg_conn = psycopg2.connect(dbname="recipe_dev", user="postgres", password="postgres")
+    except:
+        print("Unable to connect to the database.")
+    
+    cur = pg_conn.cursor()
+    try:
+        cur.execute("""SELECT * from users""")
+    except:
+        print("Can't SELECT from users")
+        
+    rows = cur.fetchall()
+    stringToReturn = "\nPG User Rows: \n"
+    for row in rows:
+        stringToReturn+= "   " + str(row)
+    
+    return stringToReturn
