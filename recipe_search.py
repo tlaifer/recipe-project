9726681c59cd48936dc50e7c39fc46fce4a72bec
Jackson <eaj4@sp21-cs411-13.cs.illinois.edu>
2021-03-20 21:14:01 -0500

    Modularize and count techniques

--- recipe_search.py
+++ recipe_search.py
@@ -3,23 +3,26 @@ import pgconnection
 import psycopg2
 import pymongo
 
+pg_cur = pgconnection.pg_setup()
+
+
 #TODO: write header
-def get_user_prefs(pg_cur, data, user):
+def get_user_prefs(data, user):
     #TODO: add error handling if data isn't a column name
     query_string = """SELECT {0} FROM users WHERE userId = {1}""".format(data, user)
     pg_cur.execute(query_string)
     
-    vetoed_array = pg_cur.fetchone()
-    vetoed_array = vetoed_array[0]
+    entity_array = pg_cur.fetchone()
+    entity_array = entity_array[0]
 
-    if (vetoed_array is None):
-        vetoed_array = []
+    if (entity_array is None):
+        entity_array = []
 
-    return vetoed_array
+    return entity_array
 
 
 #TODO: write header
-def is_vetoed(pg_cur, user, entity_array, vetoed_array):
+def is_entity_vetoed(user, entity_array, vetoed_array):
 
     if (len(entity_array) <= 0): # nothing is vetoed
         return False
@@ -34,45 +37,57 @@ def is_vetoed(pg_cur, user, entity_array, vetoed_array):
 
     return vetoed
 
-#TODO: break into smaller functions
-def main():
-    recipe_db = mongoconnection.mongo_setup()
-    pg_cur = pgconnection.pg_setup()
+
+#TODO: make global variables, write header
+def is_recipe_vetoed(recipe, vetoed_ingredients, vetoed_techniques):
+
+    recipe_ingredients = recipe['ingredients']
+    recipe_techniques = recipe['techniques']
 
     #TODO: get user 1 out of here
-    #familiar_techniques = get_user_prefs(pg_cur, 'familiarTechniques', 1)
-    #familiar_technique_count = len(familiar_techniques)
+    if (len(vetoed_ingredients) > 0): # if recipe has a vetoed ingredient
+        if (is_entity_vetoed(1, recipe_ingredients, vetoed_ingredients) == True):
+            return True
+    elif (len(vetoed_techniques) > 0): # if recipe has a vetoed technique
+        if (is_entity_vetoed(1, recipe_techniques, vetoed_techniques) == True):
+            return True
 
-    vetoed_ingredients = [] #TODO get_user_prefs(pg_cur, 'vetoedIngredients', 1)
-    vetoed_ingredient_count = len(vetoed_ingredients)
+    return False
 
-    vetoed_techniques = get_user_prefs(pg_cur, 'vetoedTechniques', 1)
-    vetoed_technique_count = len(vetoed_techniques)
+
+#TODO: write header
+def main():
+    recipe_db = mongoconnection.mongo_setup()
+
+    #TODO: get user 1 out of here
+    #familiar_techniques = get_user_prefs('familiarTechniques', 1)
+    familiar_techniques = [1,2] #TODO: fix familiar technique lookup
+    vetoed_ingredients = [] #TODO get_user_prefs('vetoedIngredients', 1)
+    vetoed_techniques = get_user_prefs('vetoedTechniques', 1)
 
     for recipe in recipe_db.find():
-        vetoed = False
+
+        # Initialize variables
         recipe_ingredients = recipe['ingredients']
         recipe_techniques = recipe['techniques']
+        ingredient_count = 0
+        extra_count = 0
+        technique_count = 0
 
-        #TODO use real user ID
-        if (vetoed_ingredient_count > 0):
-            vetoed = is_vetoed(pg_cur, 1, recipe_ingredients, vetoed_ingredients)
-            if (vetoed == True): # quit if an ingredient is vetoed
-                break
-
-        #TODO use real user ID
-        if (vetoed_technique_count > 0):
-            vetoed = is_vetoed(pg_cur, 1, recipe_techniques, vetoed_ingredients)
-            if (vetoed == True): # quit if a technique is vetoed
-                break
+        # If there is at least one veto preference, check if we should veto this recipe
+        if ((len(vetoed_ingredients) >= 0) or (len(vetoed_techniques) >= 0)):
+            if (is_recipe_vetoed(recipe, vetoed_ingredients, vetoed_techniques) == True):
+                break # recipe is vetoed
 
         for technique in recipe_techniques:
-            pass #TODO: count preferred techniques
+            if (technique in familiar_techniques):
+                technique_count += 1
 
     #TODO: evaluate ingredients passed in from search and increment ingredient and extra counts
     #TODO: build recipe dictionaries here (or make a function to build them)
 
     return
+
 
 if __name__ == '__main__':
     main()
