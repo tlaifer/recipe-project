5da73b4100216e2f49b4c0894801ac2fb693be93
Jackson <eaj4@sp21-cs411-13.cs.illinois.edu>
2021-03-22 11:36:21 -0500

    Make filter shell routine, no code yet

---
+++ recipe_filter.py
@@ -0,0 +1,24 @@
+def main():
+
+    """
+
+    Outline:
+
+    Grab recipe data from search algo and grab filters from client:
+    - Sort by using the most ingredients (e.g. vegetables are about to go bad)
+      or by using the fewest extras (e.g. I don't want to go to the store)?
+    - Technique filtering – add checkbox like "familiar techniques only"?
+    - If we want to get fancy: put ALL ingredients in ALL returned recipes into
+      local memory cache, and from there, allow user to uncheck things (e.g. one
+      of the returned recipes requires lobster and I don't want to pay for that)
+    - Any other things?
+
+    After search algo builds basic recipe dictionary by eliminating vetoed things,
+    cache the recipe dictionary locally to allow users to sort/filter the results
+
+    """
+
+    return
+
+if __name__ == '__main__':
+    main()
